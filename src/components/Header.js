aaf14a798009cbd8ad8f68ebc9d6993443da277f
Farid Murzone <faridmurzone@gmail.com>
2020-02-18 08:02:03 -0300

    Added some things

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,15 @@ import React from 'react';
 const Header = () => {
   return(
     <div className="header__container">
-
+      <div className="header__languaje">
+        <div className="">ES</div>
+        <div className="">EN</div>
+        <div className="">CAT</div>
+      </div>
+      <div className="header__nav">
+        <button>FORUM</button>
+        <button>CROWDFUNDING</button>
+      </div>
     </div>
   )
 }
--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import sign from '../assets/sign.gif';
 
 const SignUp = () => {
   return(
     <div className="signup__container">
-      Sign up...
+      <img src={sign} alt="Sign in!" />
+      <form className="">
+        <input type="text" placeholder="USERNAME" /><br />
+        <input type="email" placeholder="EMAIL" /><br />
+        <input type="text" placeholder="COUNTRY" /><br />
+        <input type="password" placeholder="PASSWORD" /><br />
+        <input type="checkbox" /><label>I accept the general conditions of use</label><br />
+        <button>Pre registration</button><br />
+        <div className="signup__registered">
+          <a href="/login"><small>I already have and account, log in</small></a>
+        </div>
+      </form>
     </div>
   )
 }

d4cf2102ff906a5067775686b3d1391737772d04
Farid Murzone <faridmurzone@gmail.com>
2020-02-23 12:15:38 -0300

    Added images from psd

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,14 @@
 import React from 'react';
-
+import es_flag from '../assets/flags/es_flag.png';
+import en_flag from '../assets/flags/en_flag.png';
+import cat_flag from '../assets/flags/cat_flag.png';
 const Header = () => {
   return(
     <div className="header__container">
       <div className="header__languaje">
-        <div className="">ES</div>
-        <div className="">EN</div>
-        <div className="">CAT</div>
+        <div className=""><img src={es_flag} alt="Español" /> </div>
+        <div className=""><img src={en_flag} alt="English" /></div>
+        <div className=""><img src={cat_flag} alt="Catalá" /></div>
       </div>
       <div className="header__nav">
         <button>FORUM</button>
--- src/components/InfoBox.js
+++ src/components/InfoBox.js
@@ -3,21 +3,23 @@ import screen from '../assets/screen1.png';
 import screen2 from '../assets/screen2.png';
 import screen3 from '../assets/screen3.png';
 import screen4 from '../assets/screen4.png';
+import kickstarter from '../assets/kikcstarter.png';
 
 const InfoBox = () => {
   return(
     <div className="infobox__container">
       <h2 className="infobox__title">Trophy <span className="infobox__title--blue">Leagues</span></h2>
       <p className="infobox__description">
-        Info... Lorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsum.
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
       </p>
       <div className="infobox__saleContainer">
         <div className="infobox__saleSubtitle">
-          Be the first
+          Be the first with
         </div>
         <div className="infobox__saleTitle">
-          <span className="infobox__saleTitle--first">Kick</span>
-          <span className="infobox__saleTitle--second">Starter</span>
+          <img src={kickstarter} alt="Start now" />
+          {/* <span className="infobox__saleTitle--first">Kick</span>
+          <span className="infobox__saleTitle--second">Starter</span> */}
         </div>
       </div>
       <div className="infobox__container">
